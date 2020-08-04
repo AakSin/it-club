6531f990752c44cdb76d2a22f6c9de6015ac0821
Aakarsh Singh <aakarshsingh0909@gmail.com>
2020-08-04 17:28:02 +0530

    Deleted old base

---
+++ turing/src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router';
+import App from './App.vue'
+Vue.use(VueRouter);
+
+const routes=[
+  {
+    
+  }
+]
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
