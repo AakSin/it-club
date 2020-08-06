990cf073bed10421997ac9380b3e33817198b424
Aakarsh Singh <aakarshsingh0909@gmail.com>
2020-08-06 18:37:25 +0530

    fix babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,28 +1,3 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"],
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          "vue-style-loader",
-          "css-loader",
-          {
-            loader: "sass-loader",
-            options: {
-              // you can also read from a file, e.g. `variables.scss`
-              // use `data` here if sass-loader version < 8
-              prependData: `
-                  $xs: "only screen and (max-width : 575px)";
-                  $sm: "only screen and (min-width : 576px)";
-                  $md: "only screen and (min-width : 768px)";
-                  $lg: "only screen and (min-width : 992px)";
-                  $xl: "only screen and (min-width : 1200px)";
-                    `
-            },
-          },
-        ],
-      },
-    ],
-  },
+  presets: ["@vue/cli-plugin-babel/preset"]
 };
