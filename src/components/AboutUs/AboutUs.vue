17cff1a318774eca63bef3454e5ad1ffa5fb9ebf
Aakarsh Singh <aakarshsingh0909@gmail.com>
2020-08-13 21:06:37 +0530

    added event heads photos

--- src/components/AboutUs/AboutUs.vue
+++ src/components/AboutUs/AboutUs.vue
@@ -3,21 +3,29 @@
     <h1 class="display-4 text-center">The f(x) Club and Turing</h1>
     <hr class="my-4" />
     <p class="lead px-5 text-center">
-     Alan Turing proposed to consider the question “can machines think?” <br><br>
-Today we say that they can and the possibilities only grow exponentially. 
-
-Welcome to the f(x) club where we inspire innovation.  
-Established in 2019, f(x) was founded by just a bunch of driven students fascinated with technology and computer sciences. Despite the pandemic, we bring to you the Turing event which gives you all a massive opportunity to showcase your technical skills and IT knowledge on a prestigious platform at LVIS Noida. 
-
-At f(x), in a tech- driven world ,we wish to create an environment where students may explore the infinite possibilities in the enigmatic world of information technology. 
-<br>
-See you at Turing!
+      Alan Turing proposed to consider the question “can machines think?”
+      <br /><br />
+      Today we say that they can and the possibilities only grow exponentially.
+      Welcome to the f(x) club where we inspire innovation. Established in 2019,
+      f(x) was founded by just a bunch of driven students fascinated with
+      technology and computer sciences. Despite the pandemic, we bring to you
+      the Turing event which gives you all a massive opportunity to showcase
+      your technical skills and IT knowledge on a prestigious platform at LVIS
+      Noida. At f(x), in a tech- driven world ,we wish to create an environment
+      where students may explore the infinite possibilities in the enigmatic
+      world of information technology.
+      <br />
+      See you at Turing!
     </p>
 
     <h1 class="display-4 text-center">Founders</h1>
     <hr class="my-4" />
     <div class="row d-flex align-items-center">
-      <div class="col-12 col-sm-6 col-lg-3 p-4 p-md-2" data-aos="flip-up" data-aos-easing="ease-out">
+      <div
+        class="col-12 col-sm-6 col-lg-3 p-4 p-md-2"
+        data-aos="flip-up"
+        data-aos-easing="ease-out"
+      >
         <div class="card text-center">
           <img
             class="card-img-top "
@@ -37,7 +45,11 @@ See you at Turing!
           </div>
         </div>
       </div>
-      <div class="col-12 col-sm-6 col-lg-3 p-4 p-md-2" data-aos="flip-up" data-aos-easing="ease-out">
+      <div
+        class="col-12 col-sm-6 col-lg-3 p-4 p-md-2"
+        data-aos="flip-up"
+        data-aos-easing="ease-out"
+      >
         <div class="card text-center">
           <img
             class="card-img-top "
@@ -55,7 +67,11 @@ See you at Turing!
           </div>
         </div>
       </div>
-      <div class="col-12 col-sm-6 col-lg-3 p-4 p-md-2" data-aos="flip-up" data-aos-easing="ease-out">
+      <div
+        class="col-12 col-sm-6 col-lg-3 p-4 p-md-2"
+        data-aos="flip-up"
+        data-aos-easing="ease-out"
+      >
         <div class="card text-center">
           <img
             class="card-img-top "
@@ -72,11 +88,14 @@ See you at Turing!
               class="fa fa-instagram"
               target="_blank"
             ></a>
-            <a href="tel:8076252192" class="fa fa-phone"></a>
           </div>
         </div>
       </div>
-      <div class="col-12 col-sm-6 col-lg-3 p-4 p-md-2" data-aos="flip-up" data-aos-easing="ease-out">
+      <div
+        class="col-12 col-sm-6 col-lg-3 p-4 p-md-2"
+        data-aos="flip-up"
+        data-aos-easing="ease-out"
+      >
         <div class="card text-center">
           <img
             class="card-img-top "
@@ -93,10 +112,129 @@ See you at Turing!
               class="fa fa-instagram"
               target="_blank"
             ></a>
-            <a href="tel:8130314810" class="fa fa-phone"></a>
           </div>
         </div>
       </div>
+    </div>
+    <h1 class="display-4 text-center">Event Heads</h1>
+    <hr class="my-4" />
+    <div class="row d-flex align-items-center justify-content-center">
+      <div
+        class="col-12 col-sm-6 col-md-4 col-xl p-4 p-md-2"
+        data-aos="flip-up"
+        data-aos-easing="ease-out"
+      >
+        <div class="card text-center">
+          <img
+            class="card-img-top "
+            src="https://lvisturing.imfast.io/turingaadityagaur.jpg"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h1 class="card-title">Aaditya Gaur</h1>
+            <h2>Gaming Head</h2>
+            <!-- <p>I created this website and I will be heading Web Development.</p> -->
+            <a
+              href="mailto:aadityagaur0405@gmail.com"
+              class="fa fa-envelope"
+            ></a>
+          </div>
+        </div>
+      </div>
+      <div
+        class="col-12 col-sm-6 col-md-4 col-xl p-4 p-md-2"
+        data-aos="flip-up"
+        data-aos-easing="ease-out"
+      >
+        <div class="card text-center">
+          <img
+            class="card-img-top "
+            src="https://lvisturing.imfast.io/turingaarav.jpg"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h1 class="card-title">Aarav Mittal</h1>
+            <h2>Gaming Head</h2>
+            <!-- <p>I created the brochure and I will be heading Disquisition</p> -->
+            <a href="https://twitter.com/aarav2703" class="fa fa-twitter"></a>
+            <a
+              href="https://www.instagram.com/adrak_mittal/"
+              class="fa fa-instagram"
+              target="_blank"
+            ></a>
+          </div>
+        </div>
+      </div>
+      <div
+        class="col-12 col-sm-6 col-md-4 col-xl p-4 p-md-2"
+        data-aos="flip-up"
+        data-aos-easing="ease-out"
+      >
+        <div class="card text-center">
+          <img
+            class="card-img-top "
+            src="https://lvisturing.imfast.io/turingmilika.jpg"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h1 class="card-title">Milika Nayak</h1>
+            <h2>Disquisition Head</h2>
+            <!-- <p>I created the brochure and I will be heading Disquisition</p> -->
+            <a href="mailto:ishan.arw@gmail.com" class="fa fa-envelope"></a>
+            <a
+              href="https://www.instagram.com/ishan.pma"
+              class="fa fa-instagram"
+              target="_blank"
+            ></a>
+          </div>
+        </div>
+      </div>
+      <div
+        class="col-12 col-sm-6 col-md-4 col-xl p-4 p-md-2"
+        data-aos="flip-up"
+        data-aos-easing="ease-out"
+      >
+        <div class="card text-center">
+          <img
+            class="card-img-top "
+            src="https://lvisturing.imfast.io/turingtanvi.jpeg"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h1 class="card-title">Tanvi Sardana</h1>
+            <h2>Presentation Head</h2>
+            <!-- <p>I created the teaser and I will be heading coding.</p> -->
+            <a
+              href="mailto:sr.official3210@gmail.com"
+              class="fa fa-envelope"
+            ></a>
+          </div>
+        </div>
+      </div>
+      <div
+        class="col-12 col-sm-6 col-md-4 col-xl p-4 p-md-2"
+        data-aos="flip-up"
+        data-aos-easing="ease-out"
+      >
+        <div class="card text-center">
+          <img
+            class="card-img-top "
+            src="https://lvisturing.imfast.io/turingyash.jpg"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h1 class="card-title">Yash Srivastava</h1>
+            <h2>Minecraft Head</h2>
+            <!-- <p>I created the brochure and I will be heading Photoshop</p> -->
+            <a
+              href="mailto:yashfiverr0901@gmail.com"
+              class="fa fa-envelope"
+            ></a>
+          </div>
+        </div>
+      </div>
+
+      
     </div>
   </div>
 </template>
@@ -111,8 +249,8 @@ export default {
   padding: 0 5vw;
 }
 
-.lead{
-   font-size: 2.2rem;
+.lead {
+  font-size: 2.2rem;
   @media #{$md} {
     font-size: 1.25rem;
   }
@@ -129,21 +267,20 @@ p {
   background-color: rgba(0, 0, 0, 0.39);
   margin-bottom: 2vh;
   transition: 0.5s;
-  cursor:pointer;
+  cursor: pointer;
   h1,
-a {
-  font-size: 3rem;
-  @media #{$md} {
-    font-size: 1.5rem;
+  a {
+    font-size: 3rem;
+    @media #{$md} {
+      font-size: 1.5rem;
+    }
   }
-}
-h2 {
-  font-size: 2.5rem;
-  @media #{$md} {
-    font-size: 1.25rem;
+  h2 {
+    font-size: 2.5rem;
+    @media #{$md} {
+      font-size: 1.25rem;
+    }
   }
-}
-  
 }
 .card:hover {
   box-shadow: 3px 10px 20px #0000007e;
@@ -155,9 +292,9 @@ h2 {
 hr {
   background-color: rgba(255, 255, 255, 0.3);
 }
-img{
-   width: 100%;
-    height: 600px;
-    object-fit: cover;
+img {
+  width: 100%;
+  height: 600px;
+  object-fit: cover;
 }
 </style>
